bf295df8fc9b172ee2460ad038532d10e5cda384
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-10-19 19:58:13 -0300

    Make the initial layout for the page
    Just added an index.html file and a script.ts file. I'm going to make
    the script code in typescript and compile it to javascript since it was
    an advice I have seen from a few developers,

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rock_paper_scisors
 Repository for the Rock Paper Scisors project of The Odin Project
+
+First project that uses JavaScript. First I will use only the console to get
+input and run the game. Latter on I shall make the page prettier by 
+introducing a GUI for the game using HTML and CSS.
---
+++ js/script.ts
@@ -0,0 +1,13 @@
+// print a welcome message to the player
+// print how the player can choose each of the three options
+
+// make a variable to record the points of the player and one for the points of the computer
+
+// make a function to get the computer choice based on a randomly generated number
+    // ...
+
+// make a function to compare the player choice with the computer choice
+    // ...
+
+// make a loop to that will run five times, one for each try
+    // 
