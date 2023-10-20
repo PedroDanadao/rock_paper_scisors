26a447dbd1e3359d1423f9394e4a39f900b28b8a
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-10-19 21:15:04 -0300

    Write the logic of the game with comments
    Just made the whole logic of the game following the tips of the
    previous lesson(problem solving).

--- js/script.ts
+++ js/script.ts
@@ -1,13 +1,52 @@
 // print a welcome message to the player
 // print how the player can choose each of the three options
 
-// make a variable to record the points of the player and one for the points of the computer
+// make a variable to record the points of the player, one for the points of the computer and 
+// one for the draws
 
 // make a function to get the computer choice based on a randomly generated number
-    // ...
+    // generate a number between 0 and 2
+    // if the number is 0 then return 'rock', if it is 1 'paper' and if it is 2 'scissors'(use a switch case)
 
 // make a function to compare the player choice with the computer choice
-    // ...
+    // get the strings of the player and the computer and attribute 'rock' to 2, 'paper' to 3 and 'scissors' to 5
+
+    // check if both numbers are equal, if so then return a 'draw' value(0)
+
+    // check if the double of the first number is less then the second number. If so then it is the case where the
+    // user chose a rock and the computer chose scissors. In that case, return the win value(1)
+
+    // check if the first number is more than the second number. If so then return a win value.
+    // if not then return a loose value -1
+
+// make a function to get a number related to a string('rock', 'paper', 'scissors')
+    // use a switch case to get the number between 2, 3 and 5
+
+// create a variable(i) to increment the number of matches between the user and the computer.
+// It will be necessary to use it alongside a while loop to deal with possible misspells
+// of the user. The variable will only increment when the user types a valid option
 
 // make a loop to that will run five times, one for each try
-    // 
+    // get the player choice from the console and put it in a variable
+
+    // check if the player choice is valid(rock, paper or scissor)
+        // if the player choice is not valid, then warn the user about it and
+        // redo the loop without incrementing the variable i
+
+    // get the computer choice
+
+    // print both choices in the console
+
+    // compare the computer choice with the player choice
+
+    // increment the player score, the computer score or the draw score based on the result of the compare function.
+    // For this, use a few math expressions based on the result of both choices
+
+    // print in the console the current score
+
+    // print in the console that a new match will start
+
+    // increment the variable i in one
+
+// compare the results between the player and the computer and announce the winner(if the results are the same)
+// then announce a draw
